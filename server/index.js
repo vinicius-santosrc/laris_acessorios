54079ec509847021a1e5e55f8f53a0464bb1a54b
Vinícius Santos <viniciusssantos.pa@gmail.com>
2025-01-06 20:32:47 -0300

    chore: added new system payment

--- server/index.js
+++ server/index.js
@@ -84,9 +84,10 @@ app.get("/config", (req, res) => {
 
 app.post("/create-payment-intent", async (req, res) => {
     try {
+        const amount = req.body.item;
         const paymentIntent = await stripe.paymentIntents.create({
-            currency: "EUR",
-            amount: 1999,
+            currency: "BRL",
+            amount: amount,
             automatic_payment_methods: { enabled: true },
         });
 
