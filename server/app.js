6517203dd0647ae61a40518e061f491612c9b397
Vinícius Santos <viniciusssantos.pa@gmail.com>
2025-04-15 16:24:09 -0300

    reverse

--- server/app.js
+++ server/app.js
@@ -34,14 +34,8 @@ const { connectToDatabase } = require('./src/config/database');
 const app = express();
 const port = process.env.PORT || 3001;
 
-//Cors settings
-const corsOptions = {
-    origin: ['https://www.larisacessorios.com.br', 'http://localhost:3000', 'https://staging-laris-acessorios-3-0.vercel.app/'],
-    optionsSuccessStatus: 200
-};
-
 // Configurações de CORS e body parser
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
