95346bd1a6734cb3081ad923453069cd7ff0e89a
Vinícius Santos <viniciusssantos.pa@gmail.com>
2025-05-24 14:37:09 -0300

    fix cors urls

--- server/app.js
+++ server/app.js
@@ -35,11 +35,21 @@ const { connectToDatabase } = require('./src/config/database');
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Configurações de CORS e body parser
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.larisacessorios.com.br'
+];
+
 app.use(cors({
-    origin: 'http://localhost:3000', // frontend
-    credentials: true               // necessário para enviar cookie
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+        return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true
 }));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
